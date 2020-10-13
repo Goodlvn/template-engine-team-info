07db88334e563a2abaa51e3cf63cc7e846ebe02c
JD Martinez <focus4ursoul@gmail.com>
2020-10-12 17:31:39 -0700

    start work space by focusing on test at a time - pass first test: instanciate Employee obj

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    };
+
+    getName (){};
+
+    getId (){};
+
+    getEmail (){};
+
+    getRole (){};
+};
+
+const Tim = new Employee ("Tim", 1, "tim@gmail.com");
+
+console.log(Tim);
+module.exports = Employee;
