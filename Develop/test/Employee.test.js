634ce539be833296dd1460c8c1904d99a5fc68be
JD Martinez <focus4ursoul@gmail.com>
2020-10-12 17:33:59 -0700

    pass test dealing with property setting

--- Develop/test/Employee.test.js
+++ Develop/test/Employee.test.js
@@ -5,23 +5,23 @@ test("Can instantiate Employee instance", () => {
   expect(typeof(e)).toBe("object");
 });
 
-// test("Can set name via constructor arguments", () => {
-//   const name = "Alice";
-//   const e = new Employee(name);
-//   expect(e.name).toBe(name);
-// });
+test("Can set name via constructor arguments", () => {
+  const name = "Alice";
+  const e = new Employee(name);
+  expect(e.name).toBe(name);
+});
 
-// test("Can set id via constructor argument", () => {
-//   const testValue = 100;
-//   const e = new Employee("Foo", testValue);
-//   expect(e.id).toBe(testValue);
-// });
+test("Can set id via constructor argument", () => {
+  const testValue = 100;
+  const e = new Employee("Foo", testValue);
+  expect(e.id).toBe(testValue);
+});
 
-// test("Can set email via constructor argument", () => {
-//   const testValue = "test@test.com";
-//   const e = new Employee("Foo", 1, testValue);
-//   expect(e.email).toBe(testValue);
-// });
+test("Can set email via constructor argument", () => {
+  const testValue = "test@test.com";
+  const e = new Employee("Foo", 1, testValue);
+  expect(e.email).toBe(testValue);
+});
 
 // test("Can get name via getName()", () => {
 //   const testValue = "Alice";
